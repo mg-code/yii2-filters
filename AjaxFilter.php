8237a584486ac16d2bb2f64f87d9e3f693cbf66d
Maris Graudins <maris.graudins91@gmail.com>
2015-06-02 12:12:06 +0300

    AjaxFilter, rename package

---
+++ AjaxFilter.php
@@ -0,0 +1,37 @@
+<?php
+namespace mgcode\filters;
+
+use Yii;
+use yii\base\ActionFilter;
+use yii\web\BadRequestHttpException;
+
+/**
+ * This filter throws an exception (BadRequestHttpException) if the applied action is receiving a non-AJAX request.
+ * To use AjaxFilter, declare it in the behaviors() method of your controller class.
+ * Use `only` and `except` properties to include/exclude actions from filter.
+ * ~~~
+ * public function behaviors()
+ * {
+ *     return [
+ *         'verbs' => [
+ *             'class' => \mgcode\filters\AjaxFilter::className(),
+ *             'only' => ['ajaxValidate'],
+ *         ],
+ *     ];
+ * }
+ */
+class AjaxFilter extends ActionFilter
+{
+    /**
+     * @param \yii\base\InlineAction $action
+     * @return boolean whether the action should continue to be executed.
+     * @throws BadRequestHttpException when the request method is not allowed.
+     */
+    public function beforeAction($action)
+    {
+        if (!Yii::$app->getRequest()->getIsAjax()) {
+            throw new BadRequestHttpException();
+        }
+        return true;
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# yii2-ajax-filter
+# AjaxFilter
 This filter throws an exception (BadRequestHttpException) if the applied action is receiving a non-AJAX request.
 
 Usage
@@ -6,15 +6,15 @@ Usage
 
 To use AjaxFilter, declare it in the `behaviors()` method of your controller class.
 
-You can use '*' to stand for all actions.
+Use `only` and `except` properties to include/exclude actions from filter.
 
 ```php
 public function behaviors()
 {
   return [
       'verbs' => [
-          'class' => \mgcode\ajaxfilter\AjaxFilter::className(),
-          'actions' => ['ajaxValidate'],
+          'class' => \mgcode\filters\AjaxFilter::className(),
+          'only' => ['ajaxValidate'],
       ],
   ];
 }
