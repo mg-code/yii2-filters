9490b2a724cf9d09705abc1fe1f86cd8f1852c8a
Maris Graudins <maris.graudins91@gmail.com>
2015-07-09 15:48:23 +0300

    DisableCsrfValidationFilter

---
+++ DisableCsrfValidationFilter.php
@@ -0,0 +1,33 @@
+<?php
+namespace mgcode\filters;
+
+use Yii;
+use yii\base\ActionFilter;
+
+/**
+ * This filter disables csrf validation. Mostly used for feedback actions (e.g. when receiving response from payment service).
+ * To use DisableCsrfValidationFilter, declare it in the behaviors() method of your controller class.
+ * Use `only` and `except` properties to include/exclude actions from filter.
+ * ~~~
+ * public function behaviors()
+ * {
+ *     return [
+ *         'verbs' => [
+ *             'class' => \mgcode\filters\DisableCsrfValidationFilter::className(),
+ *             'only' => ['feedback'],
+ *         ],
+ *     ];
+ * }
+ */
+class DisableCsrfValidationFilter extends ActionFilter
+{
+    /**
+     * @param \yii\base\InlineAction $action
+     * @return boolean whether the action should continue to be executed.
+     */
+    public function beforeAction($action)
+    {
+        $this->owner->enableCsrfValidation = false;
+        return true;
+    }
+}
--- README.md
+++ README.md
@@ -19,3 +19,25 @@ public function behaviors()
   ];
 }
 ```
+
+# DisableCsrfValidationFilter
+This filter disables csrf validation. Mostly used for feedback actions (e.g. when receiving response from payment service).
+
+Usage
+-----
+
+To use DisableCsrfValidationFilter, declare it in the `behaviors()` method of your controller class.
+
+Use `only` and `except` properties to include/exclude actions from filter.
+
+```php
+public function behaviors()
+{
+    return [
+        'verbs' => [
+            'class' => \mgcode\filters\DisableCsrfValidationFilter::className(),
+           'only' => ['feedback'],
+        ],
+    ];
+}
+```
