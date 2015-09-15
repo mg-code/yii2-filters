a8553d6c97e49763b8074ea70d8f5bfd14f25624
Maris Graudins <maris.graudins91@gmail.com>
2015-09-15 12:56:21 +0300

    fix comment

--- AjaxFilter.php
+++ AjaxFilter.php
@@ -19,6 +19,7 @@ use yii\web\BadRequestHttpException;
  *         ],
  *     ];
  * }
+ * ~~~
  */
 class AjaxFilter extends ActionFilter
 {
--- DisableCsrfValidationFilter.php
+++ DisableCsrfValidationFilter.php
@@ -18,6 +18,7 @@ use yii\base\ActionFilter;
  *         ],
  *     ];
  * }
+ * ~~~
  */
 class DisableCsrfValidationFilter extends ActionFilter
 {
